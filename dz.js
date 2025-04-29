6a550817233aa83dbb7c1985226d8961a88fc702
Kateryna <katarinashevchenko7@gmail.com>
2025-04-29 14:33:49 +0200

    first commit

---
+++ dz.js
@@ -0,0 +1,12 @@
+let value = 0;
+function summar() {
+  return function (num) {
+    value += num;
+    return value;
+  };
+}
+const sum = summar();
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7));
